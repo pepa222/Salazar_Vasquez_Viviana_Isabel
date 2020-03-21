690e328df09b3664416bc7064c0b0bcb7d781fd4
Chabelita <57578693+pepa222@users.noreply.github.com>
2020-03-21 17:41:06 -0600

    primer commit

---
+++ 21-marzo-2020.py
@@ -0,0 +1,24 @@
+simbolo="*"
+
+print()
+print("#1")
+for x in range(1,9):
+    print(simbolo*x)
+print()
+print("#2")
+print()
+for y in range(9,-1,-1):
+    print (simbolo*y)
+    
+print()
+print("#3")
+print()
+for x in range(1,9):
+    print(simbolo*x)
+for y in range (9,-1,-1):
+    print(simbolo*y)
+print()
+print("#4")
+print()
+for x in range(0,8):
+    print(simbolo*8)
